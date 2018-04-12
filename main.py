09538804509e28b0cb7b12948fbf93aa94e5e32b
Luis Angel Sanchez <sanzluisangel@icloud.com>
2018-04-12 08:30:12 -0500

    PUlled and merged

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ Created on Fri Apr  6 12:56:27 2018
 @author: sanzla_tv
 """
 
-import tensorflow as tf
-import pandas as pd
-import numpy as np
+#import tensorflow as tf
+#import pandas as pd
+#import numpy as np
 import re
 from translator import wordsToPhonetics
 
